48b3d2411391dd25eb6d7741bdc97c68cf4050c8
ibrahim0015 <muhammadibrahim15th@gmail.com>
2026-01-21 21:47:43 +0500

    edge cases handled

--- Initial tasks/task 2/storage.py
+++ Initial tasks/task 2/storage.py
@@ -1,59 +1,105 @@
 import json
 
 def show_list():
-    with open("todos.json",'r') as f:
-        data = json.load(f)
-
-    sorted_tasks = sorted(data, key=lambda task: task["priority"])
+    try:
+        with open("todos.json",'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print("File does not exists.")
+        return
+    except json.JSONDecodeError:
+        print("file is corrupted.")
+        return
+    if not data:
+        print("No tasks available.")
+        return
+    try:
+        sorted_tasks = sorted(data, key=lambda task: task["priority"])
+    except Exception as e:
+        print("Error while sorting tasks: ",e)
 
     for task in sorted_tasks:
         print(task)
 
-def add_task(title,date, priority):
-    dict={"title":title,
-          "date":date,
-          "priority":priority,
-          "done":False}
-    
-    with open("todos.json",'r')as f:
-        data = json.load(f)
 
-    data.append(dict)
+def add_task(title, date, priority):
+    new_task = {
+        "title": title,
+        "date": date,
+        "priority": priority,
+        "done": False
+    }
 
-    with open("todos.json",'w') as f:
-        json.dump(data,f,indent=2)
+    try:
+        with open("todos.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = []
+    except json.JSONDecodeError:
+        print("Todo file is corrupted.")
+        return
+
+    for task in data:
+        if task.get("title") == title:
+            print("Task with this title already exists.")
+            return
+
+    data.append(new_task)
+
+    with open("todos.json", "w") as f:
+        json.dump(data, f, indent=2)
+
     print("Task added successfully.")
 
+
 def delete_task(taskname):
-    with open("todos.json",'r') as f:
-        data = json.load(f)
-        found = False
-        newdata=[]
-        for task in data:
-            if task['title']==taskname:
-                found = True
-                continue
-            else:
-                newdata.append(task)
-        if found==False:
-            print("Task not found.")
+    try:
+        with open("todos.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print("No todo file found.")
+        return
+    except json.JSONDecodeError:
+        print("Todo file is corrupted.")
+        return
+    
+    found = False
+    newdata=[]
+    
+    for task in data:
+        if task['title']==taskname:
+            found = True
+            continue
+        else:
+            newdata.append(task)
+    if found==False:
+        print("Task not found.")
             
     if found == True:
         with open("todos.json",'w') as f:
             json.dump(newdata,f,indent=2)
         print("Task deleted successfully.")
 
+
 def mark_done(title):
-    with open("todos.json",'r') as f:
-        data=json.load(f)
-        found = False
-        for task in data:
-            if task["title"]==title:
-                task["done"]=True
-                found = True
-                break
-        if found==False:
-            print("Task not found.")
+    try:
+        with open("todos.json", "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print("No todo file found.")
+        return
+    except json.JSONDecodeError:
+        print("Todo file is corrupted.")
+        return
+    found = False
+    for task in data:
+        if task["title"]==title:
+            task["done"]=True
+            found = True
+            break
+    if found==False:
+        print("Task not found.")
+        return
     if found==True:
         with open("todos.json",'w') as f:
             json.dump(data,f,indent=2)
