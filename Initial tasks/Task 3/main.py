13d004e6b04c7f93699a61e4a92dde76f37617c4
ibrahim0015 <muhammadibrahim15th@gmail.com>
2026-01-21 22:09:42 +0500

    Update main.py

--- Initial tasks/Task 3/main.py
+++ Initial tasks/Task 3/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def clean():
     try:
-        df = pd.read_csv("C:\Code\Python\Intership\Initial tasks\Task 3\healthcare_messy_data.csv")
+        df = pd.read_csv(r"C:\Code\Python\Intership\Initial tasks\Task 3\healthcare_messy_data.csv")
         print("rows, columns before cleaning.")
         print(df.shape)
     except FileNotFoundError:
@@ -13,22 +13,28 @@ def clean():
     df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
 
     # validating the 'Email' column
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    df = df[df['Email'].str.contains(pattern, case=False, regex=True, na=False)]
-
+    if "Email" in df.columns:
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        df = df[df['Email'].str.contains(pattern, case=False, regex=True, na=False)]
+    else:
+        print("'Email' column not found.")
     #	converting dates to ISO format
-    df['Visit Date'] = pd.to_datetime(df['Visit Date'], errors='coerce')
-    df['Visit Date'] = df['Visit Date'].dt.strftime('%Y-%m-%d')
-
+    if "Visit Date" in df.columns:
+        df['Visit Date'] = pd.to_datetime(df['Visit Date'], errors='coerce')
+        df['Visit Date'] = df['Visit Date'].dt.strftime('%Y-%m-%d')
+    
     # Dropping rows with 'Nan' value in 'Condition' column
-    df= df.dropna(subset=['Condition'])
-    print("Dropped the rows with 'Nan' value in 'Condition' column.")
+    if "Condition" in df.columns:
+        df= df.dropna(subset=['Condition'])
+        print("Dropped the rows with 'Nan' value in 'Condition' column.")
 
     # Dropping rows with none value in 'Medication' column
-    df = df[~df['Medication'].str.contains('NONE')]
-    print("Dropped rows with none value in 'Medication' column")
+    if 'Medication' in df.columns:
+        df = df[~df['Medication'].str.contains('NONE', na=False)]
+        print("Dropped rows with none value in 'Medication' column")
 
     df.to_csv("Cleaned.csv")
+    
     print("Saved the cleaned file to 'Cleaned.csv")
     print("rows, columns after cleaning.")
     print(df.shape)
